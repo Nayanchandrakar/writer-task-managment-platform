cea6ccd8f5d453ae886daabcc36a3d271b752cbd
Nayan chandrakar <chandrakarnayan96@gmail.com>
2024-02-06 13:04:58 +0530

    model update

--- components/global/create-chapter-modal.tsx
+++ components/global/create-chapter-modal.tsx
@@ -6,7 +6,7 @@ import { Input } from "@components/ui/input";
 import { defaultImages } from "@constants/images";
 import { useCreateChapterModal } from "@hooks/use-create-chapter-modal";
 import { unsplash } from "@lib/unsplash";
-import { Loader2 } from "lucide-react";
+import { Check, Loader2 } from "lucide-react";
 import Image from "next/image";
 import { FC, useEffect, useState } from "react";
 
@@ -22,6 +22,11 @@ const CreateChapterModal: FC<CreateChapterModalProps> = ({}) => {
   const [Images, setImages] =
     useState<Array<Record<string, any>>>(defaultImages);
   const [IsLoading, setIsLoading] = useState<boolean>(false);
+
+  const [selectImage, setSelectedImage] = useState({
+    id: "",
+    imageUrl: "",
+  });
 
   useEffect(() => {
     const fetchImage = async () => {
@@ -48,6 +53,13 @@ const CreateChapterModal: FC<CreateChapterModalProps> = ({}) => {
     fetchImage();
   }, []);
 
+  const handleToogle = (id: string, imageUrl: string) => {
+    setSelectedImage({
+      id,
+      imageUrl,
+    });
+  };
+
   return (
     <DialogModal
       isOpen={true}
@@ -64,20 +76,36 @@ const CreateChapterModal: FC<CreateChapterModalProps> = ({}) => {
         </div>
       )}
 
-      <div className="grid grid-cols-3 gap-4 my-5">
-        {Images?.map((image) => (
-          <div className="w-full  rounded-sm overflow-hidden h-24">
-            <Image
-              src={image?.urls?.thumb}
-              alt="unsplash-image"
-              width={1000}
-              height={1000}
-              sizes="100vw"
-              className="object-cover  w-full h-full"
-            />
-          </div>
-        ))}
-      </div>
+      {!IsLoading && (
+        <div className="grid grid-cols-3 gap-3 my-5 ">
+          {Images?.map((image) => (
+            <div
+              onClick={() => handleToogle(image?.id, image?.urls?.large)}
+              className="w-full  rounded-sm overflow-hidden h-24 relative"
+            >
+              <Image
+                src={image?.urls?.thumb}
+                alt="unsplash-image"
+                width={1000}
+                height={1000}
+                sizes="100vw"
+                className="object-cover  w-full h-full"
+              />
+              <div className="w-full h-full hover:bg-white/5 absolute inset-0 rounded-sm text-sm text-white font-semibold hover:underline flex items-end p-1.5 line-clamp-1 group cursor-pointer">
+                <span className="group-hover:opacity-100 opacity-0 transition-opacity duration-200">
+                  {image?.user?.name}
+                </span>
+              </div>
+
+              {selectImage?.id === image?.id && (
+                <div className="flex items-center justify-center absolute inset-0 bg-black/10">
+                  <Check className="w-6 h-6 text-white " />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
 
       <span className="text-lg font-semibold text-slate-600">
         Chapter title
