86340f9ccaefb69ceb807df86aaecb8abb944231
Nayan chandrakar <chandrakarnayan96@gmail.com>
2024-02-27 21:20:41 +0530

    removed: env

--- lib/unsplash.ts
+++ lib/unsplash.ts
@@ -1,5 +1,5 @@
 import { createApi } from "unsplash-js";
 
 export const unsplash = createApi({
-  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEYd!,
+  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!,
 });
